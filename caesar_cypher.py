fd484c87f7390c159b6223fba7d686e505587abf
Carlos Santos <cadu.vs@hotmail.com>
2024-08-15 18:53:54 -0300

    refactor: removed old code

--- caesar_cypher.py
+++
@@ -1,9 +0,0 @@
-class CaesarCypher:
-    def __init__(self, offset: int):
-        self.offset = offset
-
-    def encrypt(self, message: str) -> str:
-        return "".join([chr(ord(c) + self.offset) for c in message])
-
-    def decrypt(self, message: str) -> str:
-        return "".join([chr(ord(c) - self.offset) for c in message])
